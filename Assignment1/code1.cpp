f0dcea06a03e00800aafee750d36b20e8671dc63
khowtung <128928645+khowtung@users.noreply.github.com>
2024-08-02 09:22:04 +0700

    Update and rename code1 to code1.cpp
    code1

---
+++ Assignment1/code1.cpp
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main () {         
+    int a = 3 ;  
+    int b = 4 ;
+    if( a > b ) {                                         
+        printf( " The numver in A is greater  than B. \n " ) ;            
+    } // end if
+    printf( " Hello world " ) ;             
+    return 0 ;
+} //end main */
+
